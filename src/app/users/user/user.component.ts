c578a1c0b17f1dbbcfdec95f7e8e41af64708453
Fahad Qayyum <fahadq@my.yorku.ca>
2020-06-26 00:03:02 -0400

    RouterLink, activatedRoute.navigate, Params, QueryParams, Fragment

--- src/app/users/user/user.component.ts
+++ src/app/users/user/user.component.ts
@@ -1,16 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {ActivatedRoute, Params} from '@angular/router';
+import {Subscription} from 'rxjs';
 
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
   styleUrls: ['./user.component.css']
 })
-export class UserComponent implements OnInit {
-  user: {id: number, name: string};
+export class UserComponent implements OnInit, OnDestroy {
+  user: { id: number, name: string };
+  paramsSubscription: Subscription;
 
-  constructor() { }
+  constructor(private activatedRoute: ActivatedRoute) {
+  }
 
   ngOnInit() {
+
+    // this should be an asynchronous task done using params observable.
+
+    this.user = {
+      id: this.activatedRoute.snapshot.params['id'],
+      name: this.activatedRoute.snapshot.params['name']
+    };
+
+    // whenever in the future that params changes it should be reflected in the component.
+    this.paramsSubscription = this.activatedRoute.params.subscribe((params: Params) => {
+      this.user.id = params['id'];
+      this.user.name = params['name'];
+    });
+  }
+
+  ngOnDestroy() {
+    this.paramsSubscription.unsubscribe();
   }
 
 }
